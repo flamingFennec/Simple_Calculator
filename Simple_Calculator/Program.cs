4888063b78326bd6a8c980bac4e6ac31025622a9
User <User@DESKTOP-B8Q1GVC>
2023-10-16 12:17:26 -0400

    Add project files.

---
+++ Simple_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Num1; double Num2; double result; string operation; Console.WriteLine("Enter the first number: "); Num1 = Convert.ToDouble(Console.ReadLine()); Console.WriteLine("Enter the second number: "); Num2 = Convert.ToDouble(Console.ReadLine()); Console.WriteLine("Enter operation (+ - / *):"); operation = Console.ReadLine(); switch (operation) { case "+": result = Num1 + Num2; break; case "-": result = Num1 - Num2; break; case "*": result = Num1 + Num2; break; case "/": result = Num1 - Num2; break; default: Console.WriteLine("Invalid operation."); return; } Console.WriteLine($"the result of {Num1} {operation} {Num2} is {result}"); Console.ReadKey();
+        }
+    }
+}
